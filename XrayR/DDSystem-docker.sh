09fd36dc953809062294c49350b322b8f2282c45
Sam-Mey <132608504+Sam-Mey@users.noreply.github.com>
2023-10-01 02:16:09 +0800

    Add files via upload

---
+++ XrayR/DDSystem-docker.sh
@@ -0,0 +1,20 @@
+### DD：
+
+sudo apt-get update
+sudo apt-get install apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+systemctl start docker
+systemctl enable docker
+
+### 安装 docker-compose
+curl -fsSL https://get.docker.com | bash -s docker
+curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
