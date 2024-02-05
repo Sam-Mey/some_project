62ff7d14ae1ff536fd11515e05c83f4ef0198666
Sam-Mey <meisongyong@gmail.com>
2024-02-06 03:49:53 +0800

    some_project

--- SSL/acme.sh
+++ SSL/acme.sh
@@ -15,11 +15,11 @@ function echo_color {
     local message=$@
     case $color in
         "green")
-            echo -e "\e[92m$message\e[0m" ;;  # 绿色
+        echo -e "\e[92m$message\e[0m" ;;  # 绿色
         "red")
-            echo -e "\e[91m$message\e[0m" ;;  # 红色
+        echo -e "\e[91m$message\e[0m" ;;  # 红色
         *)
-            echo "$message" ;;
+        echo "$message" ;;
     esac
 }
 
@@ -45,45 +45,45 @@ case $choice in
         yum install -y socat
         curl https://get.acme.sh | sh
         ln -s /root/.acme.sh/acme.sh /usr/local/bin/acme.sh
-
+        
         # 用户注册
         echo_color "green" "请输入您的邮箱地址："
         read email_address
         acme.sh --register-account -m "$email_address"
-
+        
         # 开始申请证书
         echo_color "green" "请输入您的域名："
         read your_domain
-        acme.sh --issue -d "$your_domain" --standalone -k ec-256 
-
+        acme.sh --issue -d "$your_domain" --standalone -k ec-256
+        
         # 安装证书到路径
-        acme.sh --installcert -d "$your_domain" --ecc --key-file /root/server.key --fullchain-file /root/server.crt
+        acme.sh --installcert -d "$your_domain" --ecc --key-file /root/private.key --fullchain-file /root/certificate.crt
         echo_color "green" "证书申请成功，并已安装到 root 目录下；您可以手动更改使用位置！"
-        ;;
-
+    ;;
+    
     2)  # Ubuntu/Debian
         echo_color "green" "正在安装必要的依赖和获取证书..."
         apt-get install -y socat
         curl https://get.acme.sh | sh
         ln -s /root/.acme.sh/acme.sh /usr/local/bin/acme.sh
-
+        
         # 用户注册
         echo_color "green" "请输入您的邮箱地址："
         read email_address
         acme.sh --register-account -m "$email_address"
-
+        
         # 开始申请证书
         echo_color "green" "请输入您的域名："
         read your_domain
-        acme.sh --issue -d "$your_domain" --standalone -k ec-256 
-
+        acme.sh --issue -d "$your_domain" --standalone -k ec-256
+        
         # 安装证书到路径（默认 root 目录）
-        acme.sh --installcert -d "$your_domain" --ecc --key-file /root/server.key --fullchain-file /root/server.crt
+        acme.sh --installcert -d "$your_domain" --ecc --key-file /root/private.key --fullchain-file /root/certificate.crt
         echo_color "green" "证书申请成功，并已安装到 root 目录下；您可以手动更改使用位置！"
-        ;;
-
+    ;;
+    
     *)  # 无效选择
         echo_color "red" "错误：无效的系统选择!"
         exit 1
-        ;;
+    ;;
 esac
